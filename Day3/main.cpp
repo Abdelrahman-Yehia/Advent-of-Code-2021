6ab411b31ee69cb2ebe5b8ae8f6b6def6b452000
Abdelrahman-Yehia <a.k.yehia3010@gmail.com>
2021-12-03 08:29:01 +0200

    Day3

--- Day3/main.cpp
+++ Day3/main.cpp
@@ -5,13 +5,161 @@ using namespace std;
 
 const int N = 2e5 + 5;
 
+const long long mod = 1e9+7;
 
-void part1(){
-    cout << ans << endl;
+long long power(long long a, long long  b, long long mod)
+{
+    if(!b)
+        return 1ll;
+    if(b == 1)
+        return a%mod;
+    long long r = power(a, b/2ll,mod)%mod;
+    if(b%2ll)
+        return ((r*(a%mod)%mod)*r)%mod;
+    else
+        return (r*r)%mod;
+
 }
 
-void part2(){
-    cout << ans << endl;
+
+void part1(vector<string> input){
+    bitset<30> gamma;
+    bitset<30> epsilon;
+
+    int len = input[0].length();
+    for(int i = 0;i < len;i++){
+        int zeros = 0, ones = 0;
+        for(int j = 0;j < input.size();j++){
+            if(input[j][len-1-i] == '1')
+                ones++;
+            else zeros++;
+        }
+        int maxi = 1, mini = 0;
+        if(zeros>ones){
+            maxi = 0;
+            mini = 1;
+        }
+
+        gamma[i] = maxi;
+        epsilon[i] = mini;
+    }
+
+    long long gammaVal = 0;
+    long long epsVal = 0;
+
+    for(int i = 0;i<30;i++){
+        gammaVal += gamma[i] * power(2,i,mod);
+        epsVal += epsilon[i] * power(2,i,mod);
+    }
+
+
+    long long ans = gammaVal * epsVal;
+    cout << "Part 2: " << ans << endl;
+}
+
+
+long long calcO2(vector<string> input){
+
+    int len = input[0].length();
+    int removed = 0;
+    while(true){
+        for(int i = 0;i<len;i++){
+            int zeros = 0, ones = 0;
+            for(int j = 0;j < input.size();j++){
+                if(input[j] == "#") {
+                    continue;
+                }
+                if(input[j][i] == '1') {
+                    ones++;
+                }
+                else zeros++;
+            }
+            int maxi;
+            if(zeros <= ones){
+                maxi = 1;
+            }
+            else maxi = 0;
+            for (auto &j : input) {
+                if(j == "#") {
+                    continue;
+                }
+                if(removed == input.size()-1) {
+                    break;
+                }
+                if(j[i] - '0' != maxi){
+                    j = "#";
+                    removed++;
+                }
+            }
+        }
+        if(removed == input.size() - 1) {
+            break;
+        }
+    }
+
+    long long o2 = 0;
+    for(int i = 0;i < input.size();i++){
+        if(input[i] == "#") {
+            continue;
+        }
+
+        for(int j = 0; j<len;j++){
+            int bit = input[i][len-j-1] - '0';
+            o2 += power(2,j,mod) * bit;
+        }
+    }
+    return o2;
+}
+
+
+long long calcCo2(vector<string> input){
+
+    int len = input[0].length();
+    int removed = 0;
+    while(true){
+        for(int i = 0;i<len;i++){
+            int zeros = 0, ones = 0;
+            for(int j = 0;j<input.size();j++){
+                if(input[j] == "#"){
+                    continue;
+                }
+                if(input[j][i] == '1')
+                    ones++;
+                else zeros++;
+            }
+            int mini;
+            if(zeros <= ones){
+                mini = 0;
+            }
+            else mini = 1;
+            for (auto &j : input) {
+                if(j == "#")
+                    continue;
+                if(removed == input.size() - 1)
+                    break;
+                if(j[i] - '0' != mini){
+                    j = "#";
+                    removed++;
+                }
+            }
+        }
+        if(removed == input.size() - 1)
+            break;
+    }
+
+    long long co2 = 0;
+    for(int i = 0;i<input.size();i++){
+        if(input[i] == "#")
+            continue;
+        for(int j = 0; j<len;j++){
+            int bit = input[i][len-j-1] - '0';
+            co2 += power(2,j,mod) * bit;
+        }
+    }
+    return co2;
+}
+void part2(vector<string> input){
+    cout << "Part 2: " << calcO2(input) * calcCo2(input) << endl;
 }
 
 int main() {
@@ -21,8 +169,16 @@ int main() {
     // int t;
     //cin >> t;
     //while(t--)
-    part1();
-    part2();
+    vector<string> numbers;
+    while(true){
+        string in;
+        cin >> in;
+        if(in == "#")
+            break;
+        numbers.push_back(in);
+    }
+    part1(numbers);
+    part2(numbers);
 
     return 0;
 }
